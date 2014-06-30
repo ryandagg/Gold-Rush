4fb9c02301c71c12e7b36a84cc1e94ad2515e55a
ryandagg <ryan.dagg@gmail.com>
2014-06-30 15:31:10 -0600

    Added input/submit and prevented side effects

--- main.js
+++ main.js
@@ -6,28 +6,48 @@ http://malialitman.files.wordpress.com/2014/04/red-x.png
 $(document).on('ready', function() {
 	// set variables used throughout
 	var clickCounter = 0;
-	
+	var submitOpen = false;
+
 	// remove the appended divs
 	$(document).on('click', '.x-image', function(e) {
 		e.stopPropagation();
 		$(this).remove();
 	});
 
+	$(document).on('click', 'input', function(e) {
+		e.stopPropagation();
+	});
 
-	// Add Xs
+	$(document).on('click', 'button', function(e) {
+		e.stopPropagation();
+	});
+
+
+	// Enter a marker
 	$(document).on('click', function(e) {
 
-		clickCounter++;
-		// Pulling x & y courdinates from document and saving to a variable.
-	  	var clickPosition = [e.pageX, e.pageY]
+		if(!submitOpen){
+			clickCounter++;
+			// Pulling x & y courdinates from document and saving to a variable.
+		  	var clickPosition = [e.pageX, e.pageY]
 
-	  	// inserting a div with image at the position clicked
-	  	$(".main-wrapper").append("<div class='x-image tooltip marker" + clickCounter + "'><img src='http://malialitman.files.wordpress.com/2014/04/red-x.png'></div>");
-	  	$(".marker" + clickCounter).css({
-	  		"position": "absolute",
-	  		"top": clickPosition[1] - 20,
-	  		"left": clickPosition[0] - 22
-  		});
+		  	// inserting an X (div with image) at the position clicked
+		  	$(".main-wrapper").append("<div title='fake user text' class='x-image tooltip marker" + clickCounter + "'><img src='http://malialitman.files.wordpress.com/2014/04/red-x.png'></div>");
+		  	$(".marker" + clickCounter).css({
+		  		"position": "absolute",
+		  		"top": clickPosition[1] - 20,
+		  		"left": clickPosition[0] - 22
+	  		});
+
+		  	//What are we doing here?
+		  	$('.main-wrapper').append("<div class='note-input" + clickCounter + "'><input type='textarea' value='Enter your note here...'><button>Submit</button></div>")
+		  	$(".note-input" + clickCounter).css({
+		  		"position": "absolute",
+		  		"top": clickPosition[1] + 25,
+		  		"left": clickPosition[0] - 22
+	  		});
+	  		submitOpen = true;
+		}
  	})
 
 
