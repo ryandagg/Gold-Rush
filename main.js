1ee1ae6f6f299cda6e2d274c227ab7889e8d81d7
ryandagg <ryan.dagg@gmail.com>
2014-06-30 14:33:35 -0600

    Added x creation and deletion

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+/* Paste bin:
+http://malialitman.files.wordpress.com/2014/04/red-x.png
+"https://raw.githubusercontent.com/RefactorU/exercise-starters/master/client-js/gold-rush/topo-co.jpg"
+*/
+
 $(document).on('ready', function() {
-  
+	// set variables used throughout
+	var clickCounter = 0;
+	
+	// remove the appended divs
+	$(document).on('click', '.x-image', function(e) {
+		e.stopPropagation();
+		$(this).remove();
+	});
+
+
+	// Add Xs
+	$(document).on('click', function(e) {
+
+		clickCounter++;
+		// Pulling x & y courdinates from document and saving to a variable.
+	  	var clickPosition = [e.pageX, e.pageY]
+
+	  	// inserting a div with image at the position clicked
+	  	$(".main-wrapper").append("<div class='x-image tooltip marker" + clickCounter + "'><img src='http://malialitman.files.wordpress.com/2014/04/red-x.png'></div>");
+	  	$(".marker" + clickCounter).css({
+	  		"position": "absolute",
+	  		"top": clickPosition[1] - 20,
+	  		"left": clickPosition[0] - 22
+  		});
+ 	})
+
+
+
+
+
 });
